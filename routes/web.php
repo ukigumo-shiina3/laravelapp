48fb118862a64be5e540f82f39b6f337ed7d9505
Daisuke Tanaka <daisukeyao@outlook.jp>
2021-01-21 20:13:55 +0900

    [Add]PHPテンプレート

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello', 'App\Http\Controllers\HelloController@index');
-
+Route::get('hello', function() {
+    return view('hello.index');
+});
