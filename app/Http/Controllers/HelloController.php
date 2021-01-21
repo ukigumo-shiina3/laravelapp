7657a106c9ba4b5c944e564c196b2c0df4bb5b47
Daisuke Tanaka <daisukeyao@outlook.jp>
2021-01-21 17:49:24 +0900

    [Add]request/responseオブジェクト

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,28 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
 
-    public function __invoke() { //アクションメソッドの代わりに_「_invoke」を使ってシングルアクション化
+    public function index (Request $request, Response $response) {
 
-    return <<<EOF
-
-    <html>
-    <head>
-    <title>Hello</title>
-    <style>
-    body {font-size:16pt; color:#999; }
-    h1 { font-size:30pt; text-align:right; color:#eee;
-        margin:-15px 0px 0px 0px; }
-    </style>
-    </head>
-    <body>
-        <h1>Single Action</h1>
-        <p>これはシングルコントローラーのアクションです。</p>
-    </body>
-    </html>
-    EOF;
+$html = <<<EOF
+<html>
+<head>
+<title>Hello/index</title>
+<style>
+body {font-size:16pt; color:#999; }
+h1 { font-size:120pt; text-align:right; color #fafafa;
+    margin:-50px 0px -120px 0px; }
+</style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <h3>Request</h3>
+    <pre>{$request}</pre>
+    <h3>Resposnse</h3>
+    <pre>{$response}</pre>
+</body>
+</html>
+EOF;
+    $response->setContent($html); //引数の値にコンテンツを変更する
+    return $response; 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello', 'App\Http\Controllers\HelloController');
+Route::get('/hello', 'App\Http\Controllers\HelloController@index');
 
