9654ece06e3a846c8b8901435475cdca09044e8b
Daisuke Tanaka <daisukeyao@outlook.jp>
2021-01-24 14:13:24 +0900

    [Add]レイアウト

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,23 +1,18 @@
-<html>
-<head>
-    <title>Hello/index</title>
-    <style>
-    body {font-size:16pt; color:#999; }
-    h1 { font-size:50pt; text-align:right; color #f6f6f6;
-        margin:-20px 0px -30px 0px; letter-spacing:-4pt;}
-    </style>
-</head>
-<body>
-    <h1>Blade/Index</h1>
-    <p>&#064;forディレクティブの例</p> 
-    @foreach ($data as $item)
-    @if ($loop->first)
-    <p>*データ一覧</p><ul> 
-    @endif
-    <li>No, {{$loop->iteration}}. {{$item}}</li>
-    @if ($loop->last)
-    </ul><p>------ここまで</p>
-    @endif
-    @endforeach
-</body>
-</html> 
+@extends('layouts.helloapp')
+
+@section('title', 'index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+copyright 2020 tuyano.
+@endsection
+
