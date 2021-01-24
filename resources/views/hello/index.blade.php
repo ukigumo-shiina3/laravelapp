6378410b618b92ef2a2172358768f074ab5f598f
Daisuke Tanaka <daisukeyao@outlook.jp>
2021-01-24 10:36:45 +0900

    [Add]フォーム

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,6 +9,11 @@
 </head>
 <body>
     <h1>Blade/Index</h1>
-    <p>{{$msg}}</p> <!--bladeテンプレートでは変数をテンプレートに埋め込める -->
+    <p>{{$msg}}</p> 
+    <form method="POST" action="/hello">
+        @csrf
+        <input type="text" name ="msg">
+        <input type="submit">
+    </form>
 </body>
 </html> 
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/hello/{id?}', 'App\Http\Controllers\HelloController@index');
+Route::get('/hello/', 'App\Http\Controllers\HelloController@index');
+Route::post('/hello/', 'App\Http\Controllers\HelloController@post');
