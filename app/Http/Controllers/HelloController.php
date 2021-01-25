49c460377f227d370858d024129bf080549622a5
Daisuke Tanaka <daisukeyao@outlook.jp>
2021-01-25 19:24:56 +0900

    [Add]@eachディレクティブ

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,7 +9,11 @@ class HelloController extends Controller
 {
         public function index()
     {
-        $data = ['one', 'two', 'three', 'four', 'five'];
+        $data = [
+            ['name'=>'山田太郎', 'mail'=>'taro@yamada'],
+            ['name'=>'田中花子', 'mail'=>'hanako@flower'],
+            ['name'=>'鈴木幸子', 'mail'=>'sachi@happy'],
+        ];
         return view('hello.index', ['data'=>$data]); 
     }
 }
