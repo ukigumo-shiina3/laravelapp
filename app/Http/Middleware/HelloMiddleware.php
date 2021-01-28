7a9876ff11411569f8b7707d4cc274c7084d4298
Daisuke Tanaka <daisukeyao@outlook.jp>
2021-01-28 17:49:09 +0900

    [Add]HelloMiddleware

---
+++ app/Http/Middleware/HelloMiddleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class HelloMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $data = [
+            ['name'=>'taro', 'mail'=>'taro@yamada'],
+            ['name'=>'hanako', 'mail'=>'hanako@flower'],
+            ['name'=>'sachiko', 'mail'=>'sachi@happy'],
+        ];
+        $repuest->merge(['data=>$data']);
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/hello/', 'App\Http\Controllers\HelloController@index');
-Route::post('/hello/', 'App\Http\Controllers\HelloController@post');
+// use \App\Http\Middleware\HelloMiddleware;
+Route::get('/hello', 'App\Http\Controllers\HelloController@index')
+    ->middleware(HelloMiddleware::class);
+Route::post('/hello', 'App\Http\Controllers\HelloController@post');
