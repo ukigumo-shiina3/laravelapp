07fb4adb0e9977761c68ffcfa2cc36eb08d09ad2
Daisuke Tanaka <daisukeyao@outlook.jp>
2021-01-29 20:46:33 +0900

    [Update]HelloMiddleware

--- app/Http/Middleware/HelloMiddleware.php
+++ app/Http/Middleware/HelloMiddleware.php
@@ -3,18 +3,17 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 
 class HelloMiddleware
 {
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next)
     {
-        $data = [
-            ['name'=>'taro', 'mail'=>'taro@yamada'],
-            ['name'=>'hanako', 'mail'=>'hanako@flower'],
-            ['name'=>'sachiko', 'mail'=>'sachi@happy'],
-        ];
-        $repuest->merge(['data=>$data']);
-        return $next($request);
+        $response = $next($request);
+        $content = $response->content();
+        $pattern = '/<middleware>(.*)<\/middleware>/i';
+        $replace = '<a href="http://$1">$1</a>';
+        $content = preg_replace($pattern, $replace, $content);
+        $response->setContent($content);
+        return $response;
     }
 }
